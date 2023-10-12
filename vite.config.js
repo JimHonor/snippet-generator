97eff216d388211b3f6ca81fee9c39464a506176
Ye <ye66169@gmail.com>
2023-10-12 17:46:14 +0800

    fix: url errors

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
   return {
+    base: mode === "development" ? "/" : "/snippet-generator/",
     plugins: [react()],
     build: {
       sourcemap: mode === "development",
